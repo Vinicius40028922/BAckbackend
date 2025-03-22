af48e39a763caf96c4585c091b014137eae59d82
Vinicius40028922 <magalhaesv335@gmail.com>
2025-03-22 13:04:56 -0300

    criação de cadastro

--- setup/urls.py
+++ setup/urls.py
@@ -17,13 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from todos.views import home
-from todos.views import todo_list
 
-urlpatterns = [
-    
-    path("", home),
-    path("admin/", todo_list),
-]
+from todos.views import TodoListView
+from todos.views import TodoCreateView
+
+
+
+urlpatterns = [ 
+    path( "admin/" , admin. site. urls ), 
+    path( "", TodoListView. as_view (), name = "todo_list"),
+    path("create",TodoCreateView.as_view(),name = "todo_create")]
+
 
 
--- todos/views.py
+++ todos/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo    
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+
+
 
 def home(request):
     return render(request, "todos/home.html")
 
 def todo_list(request):
     todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos":todos})
+    return render(request, "todo_list.html", {"todos":todos})
 
 def todo_list_old(request):
     nome = 'Rodrigo'
     alunos = ['1.Ana', '2.José', '3.Bia']
-    return render(request, "todos/todo_list.html", {"nome":nome, "lista_alunos":alunos})
+    return render(request, "todo_list.html", {"nome":nome, "lista_alunos":alunos})
 
+class TodoListView(ListView):
+    model = Todo
 
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title","deadline",]
+    success_url = reverse_lazy("todo_list")
 
 
 # Create your views here.
